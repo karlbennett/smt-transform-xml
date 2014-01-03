9d9650ebaaed3858f3f193923ba1ca455583ff34
Karl Bennett <karlbennett@orcon.net.nz>
2014-01-03 13:07:04 +1300

    Updated the Antlr4StreamTransformer to be an Antlr4StringTransformer.

--- src/main/java/shiver/me/timbers/transform/xml/XmlTransformer.java
+++ src/main/java/shiver/me/timbers/transform/xml/XmlTransformer.java
@@ -2,7 +2,7 @@ package shiver.me.timbers.transform.xml;
 
 import shiver.me.timbers.antlr4.xml.XMLParser;
 import shiver.me.timbers.transform.Transformations;
-import shiver.me.timbers.transform.antlr4.Antlr4StreamTransformer;
+import shiver.me.timbers.transform.antlr4.Antlr4StringTransformer;
 import shiver.me.timbers.transform.antlr4.TokenTransformation;
 
 /**
@@ -10,7 +10,7 @@ import shiver.me.timbers.transform.antlr4.TokenTransformation;
  * These names can be found in the {@link XMLParser#tokenNames} and {@link XMLParser#ruleNames}
  * arrays.
  */
-public class XmlTransformer extends Antlr4StreamTransformer<XMLParser> {
+public class XmlTransformer extends Antlr4StringTransformer<XMLParser> {
 
     public XmlTransformer() {
         super(new XmlParserBuilder());
