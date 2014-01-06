4d34320a30a7ded068ff972dfb77364cd65c4396
Karl Bennett <karlbennett@orcon.net.nz>
2014-01-06 16:45:44 +1300

    Updated the project to use the new package structure.

--- src/test/java/shiver/me/timbers/transform/xml/TransformationsConstants.java
+++ src/test/java/shiver/me/timbers/transform/xml/TransformationsConstants.java
@@ -3,7 +3,7 @@ package shiver.me.timbers.transform.xml;
 import org.antlr.v4.runtime.RuleContext;
 import org.antlr.v4.runtime.Token;
 import shiver.me.timbers.antlr4.xml.XMLParser;
-import shiver.me.timbers.transform.IterableTransformations;
+import shiver.me.timbers.transform.iterable.IterableTransformations;
 import shiver.me.timbers.transform.Transformations;
 import shiver.me.timbers.transform.antlr4.CompositeTokenTransformation;
 import shiver.me.timbers.transform.antlr4.CompoundTransformations;
--- src/test/java/shiver/me/timbers/transform/xml/XmlTransformerTest.java
+++ src/test/java/shiver/me/timbers/transform/xml/XmlTransformerTest.java
@@ -1,7 +1,7 @@
 package shiver.me.timbers.transform.xml;
 
 import org.junit.Test;
-import shiver.me.timbers.transform.IterableTransformations;
+import shiver.me.timbers.transform.iterable.IterableTransformations;
 import shiver.me.timbers.transform.antlr4.TokenTransformation;
 import shiver.me.timbers.transform.language.test.TransformerTestTemplate;
 
